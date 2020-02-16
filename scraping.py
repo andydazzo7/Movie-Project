60f1ee4c71d4adc6e17c38b69dad7c8bc7b9a708
Andy Dazzo <andydazzo@Andys-MacBook-Pro-2.local>
2020-02-16 17:59:50 -0500

    two theaters

--- scraping.py
+++ scraping.py
@@ -53,116 +53,128 @@ def imdbScrape(movie, showtimes):
     #wait = WebDriverWait(driver2, 10)
     #element = wait.until(EC.visibility_of_eleme
     # nt_located((By.CLASS_NAME, 'result_text')))
-    a = driver2.find_element_by_class_name('result_text')
-    link = a.find_element_by_tag_name('a')
-    main_window = driver2.current_window_handle
-    ActionChains(driver2) \
-    .key_down(Keys.COMMAND) \
-    .click(link) \
-    .key_up(Keys.COMMAND) \
-    .perform()
-    driver2.switch_to.window(driver2.window_handles[1])
-    time.sleep(10)
-    rating = driver2.find_element_by_class_name('ratingValue')
-    imdbRating = rating.find_element_by_tag_name('span')
-    for s in showtimes:
-        ratings.append(imdbRating.text)
-    print('Rating: ' + imdbRating.text)
-    summary = driver2.find_elements_by_class_name('credit_summary_item')
-    peopleInolve = " "
-    for s in summary:
-        category = s.find_element_by_tag_name('h4')
-        peopleInolve += category.text + ": " 
-        print(category.text)
-        items = s.find_elements_by_tag_name('a')
-        for i in items: 
-            peopleInolve += i.text + ", "
-            print(i.text)
-    for s in showtimes:
-        peopleInolved.append(peopleInolve)
-    bigDets = driver2.find_element_by_id('titleDetails')
-    details = bigDets.find_elements_by_class_name('txt-block')
-    prodsBig = " "
-    for d in details:
-        try:
-            budgetcount = 0
-            h4 = d.find_element_by_tag_name('h4')
-            if(h4.text == 'Gross USA:'):
-                budget = d.text
-                for s in showtimes:
-                    earnings.append(budget)
-                print( 'budget: ' + budget)
-            elif(h4.text == 'Release Date:'):
-                release = d.text
-                for s in showtimes:
-                    releaseDates.append(release)
-                print('release: ' + release)
-            elif(h4.text == 'Production Co:'):
-                prods = d.find_elements_by_tag_name('a')
-                for p in prods:
-                    prodsBig = ProdsBig + p.text + ", "
-                    print('Prod: ' + p.text)
-                for s in showtimes:
-
-                    Productions.append(prodsBig)
-           
-        except:
-            pass
-    if( len(earnings) < len(releaseDates)):
+    try:
+        a = driver2.find_element_by_class_name('result_text')
+        link = a.find_element_by_tag_name('a')
+        main_window = driver2.current_window_handle
+        ActionChains(driver2) \
+        .key_down(Keys.COMMAND) \
+        .click(link) \
+        .key_up(Keys.COMMAND) \
+        .perform()
+        driver2.switch_to.window(driver2.window_handles[1])
+        time.sleep(10)
+        rating = driver2.find_element_by_class_name('ratingValue')
+        imdbRating = rating.find_element_by_tag_name('span')
         for s in showtimes:
-            earnings.append(0)
-    barItems = driver2.find_elements_by_class_name('titleReviewBarItem')
-    for b in barItems: 
-        print b.text
-        bsmall = b.text
-        if(len(re.findall('Metascore', bsmall)) > 0):
-            metascore = re.sub('[^0-9]','', bsmall)
-            print('meta ' + metascore)
-            for s in showtimes: 
-                metascores.append(metascore)
-        elif(len(re.findall('Popularity', bsmall)) > 0):
-            if ('(') in bsmall:
-                Result = bsmall[:bsmall.find('(') + 1]
-                popRating = re.sub('[^0-9]','', Result)
-            else:
-                popRating = re.sub('[^0-9]','', bsmall)
-            print('pop:' + popRating)
-            for s in showtimes: 
-                popularities.append(popRating)
-    CastList = driver2.find_element_by_class_name('cast_list')
-    people = CastList.find_elements_by_class_name('primary_photo')
-    StarPower = 0
-    for i in range(9):
-        link = people[i]
-        link1 = link.find_element_by_tag_name('a').get_attribute('href')
-        webdriver = "/Users/andydazzo/Desktop/chromedriver"
-        driver5 = Chrome(webdriver,options=chrome_options)
-        driver5.get(link1)
-        try:
-            meter = driver5.find_element_by_id('meterHeaderBox')
-            rating = meter.find_element_by_tag_name('a').text
-        
-            if(rating == 'SEE RANK'):
-                rating = 10000
-            elif(rating == 'Top 5000'):
-                rating = 5000
-            elif(rating == 'Top 500'):
-                rating = 500
-            print(rating)
-            StarPower+= (int)(rating)
-        except:
-            StarPower += 20000
-        driver5.close()
-        #driver2.close()
-    for s in showtimes:
-        starpowers.append(StarPower)
-    driver2.switch_to_window(main_window)
+            ratings.append(imdbRating.text)
+        print('Rating: ' + imdbRating.text)
+        summary = driver2.find_elements_by_class_name('credit_summary_item')
+        peopleInolve = " "
+        for s in summary:
+            category = s.find_element_by_tag_name('h4')
+            peopleInolve += category.text + ": " 
+            print(category.text)
+            items = s.find_elements_by_tag_name('a')
+            for i in items: 
+                peopleInolve += i.text + ", "
+                print(i.text)
+        for s in showtimes:
+            peopleInolved.append(peopleInolve)
+        bigDets = driver2.find_element_by_id('titleDetails')
+        details = bigDets.find_elements_by_class_name('txt-block')
+        prodsBig = " "
+        for d in details:
+            try:
+                budgetcount = 0
+                h4 = d.find_element_by_tag_name('h4')
+                if(h4.text == 'Gross USA:'):
+                    budget = d.text
+                    for s in showtimes:
+                        earnings.append(budget)
+                    print( 'budget: ' + budget)
+                elif(h4.text == 'Release Date:'):
+                    release = d.text
+                    for s in showtimes:
+                        releaseDates.append(release)
+                    print('release: ' + release)
+                elif(h4.text == 'Production Co:'):
+                    prods = d.find_elements_by_tag_name('a')
+                    for p in prods:
+                        prodsBig = ProdsBig + p.text + ", "
+                        print('Prod: ' + p.text)
+                    for s in showtimes:
+
+                        Productions.append(prodsBig)
+            
+            except:
+                pass
+        if( len(earnings) < len(releaseDates)):
+            for s in showtimes:
+                earnings.append(0)
+        barItems = driver2.find_elements_by_class_name('titleReviewBarItem')
+        for b in barItems: 
+            print b.text
+            bsmall = b.text
+            if(len(re.findall('Metascore', bsmall)) > 0):
+                metascore = re.sub('[^0-9]','', bsmall)
+                print('meta ' + metascore)
+                for s in showtimes: 
+                    metascores.append(metascore)
+            elif(len(re.findall('Popularity', bsmall)) > 0):
+                if ('(') in bsmall:
+                    Result = bsmall[:bsmall.find('(') + 1]
+                    popRating = re.sub('[^0-9]','', Result)
+                else:
+                    popRating = re.sub('[^0-9]','', bsmall)
+                print('pop:' + popRating)
+                for s in showtimes: 
+                    popularities.append(popRating)
+        CastList = driver2.find_element_by_class_name('cast_list')
+        people = CastList.find_elements_by_class_name('primary_photo')
+        StarPower = 0
+        for i in range(9):
+            link = people[i]
+            link1 = link.find_element_by_tag_name('a').get_attribute('href')
+            webdriver = "/Users/andydazzo/Desktop/chromedriver"
+            driver5 = Chrome(webdriver,options=chrome_options)
+            driver5.get(link1)
+            try:
+                meter = driver5.find_element_by_id('meterHeaderBox')
+                rating = meter.find_element_by_tag_name('a').text
+            
+                if(rating == 'SEE RANK'):
+                    rating = 10000
+                elif(rating == 'Top 5000'):
+                    rating = 5000
+                elif(rating == 'Top 500'):
+                    rating = 500
+                print(rating)
+                StarPower+= (int)(rating)
+            except:
+                StarPower += 20000
+            driver5.close()
+            #driver2.close()
+        for s in showtimes:
+            starpowers.append(StarPower)
+        driver2.switch_to_window(main_window)
+    except:
+        for s in showtimes: 
+            starpowers.append(None)
+            popularities.append(None)
+            earnings.append(None)
+            releaseDates.append(None)
+            metascores.append(None)
+            ratings.append(None)
+            peopleInolved.append(None)
+            print("No IMDB DATA FOUND")
     driver2.quit()
 
     
-
+url1 = "https://www.fandango.com/amc-sunset-5-aacoz/theater-page"
+url2 = "https://www.fandango.com/amc-sunset-5-aacoz/theater-page"
 #add path for local machine
-def scraper():
+def scraper(url):
     chrome_options = Options()
     #chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-gpu")
@@ -171,8 +183,11 @@ def scraper():
    # driver = webdriver.Chrome(options=chrome_options)
     webdriver = "/Users/andydazzo/Desktop/chromedriver"
     driver = Chrome(webdriver, options=chrome_options)
-    url = "https://www.fandango.com/amc-lincoln-square-13-aabqi/theater-page"
+   
+    #NYCurl = "https://www.fandango.com/amc-lincoln-square-13-aabqi/theater-page"
     driver.get(url)
+    wait = WebDriverWait(driver, 15)
+    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'fd-movie')))
     movies = driver.find_elements_by_class_name('fd-movie')
     times = []
     titles = []
@@ -180,13 +195,14 @@ def scraper():
     RRGs= []
     amenitiesBig = []
     timesTaken = []
+    # range 5 for sunset AMC, range 12 for nYC AMC
     for i in range(5):
-        title = movies[i].find_element_by_class_name("dark")
+        title = movies[i].find_element_by_tag_name("h3")
         print(title.text)
         RatingRuntimeGenre = movies[i].find_element_by_class_name("fd-movie__rating-runtime")
         print(RatingRuntimeGenre.text)
         showtimes = movies[i].find_elements_by_class_name("fd-movie__btn-list-item")
-        imdbScrape(title.text, showtimes)
+       # imdbScrape(title.text, showtimes)
         count = 0
         for s in showtimes:
             count = count + 1
@@ -269,14 +285,14 @@ def scraper():
         'time': times,
         #'amenities' : amenitiesBig,
         'price' :prices,
-        'peopleInolved':  peopleInolved,
-        'IMDB rating': ratings,
-        'Earnings': earnings,
-        'Release Date': releaseDates,
+        #'peopleInolved':  peopleInolved,
+        #'IMDB rating': ratings,
+        #'Earnings': earnings,
+        #'Release Date': releaseDates,
         'percent full': percentsfull,
-        'Star Power' : starpowers,
-        'Popularity': popularities,
-        'MetaCritic Score': metascores,
+        #'Star Power' : starpowers,
+        #'Popularity': popularities,
+        #'MetaCritic Score': metascores,
         'Time Taken': timesTaken,
         'Money Missed' : moneyMissed,
         'Open Seats' : opens,
@@ -296,9 +312,10 @@ def scraper():
         print("csv failed")
 if __name__ == "__main__":
     print("started")
-    scraper()
+    scraper(url1)
     count1 = 0 
-    schedule.every(15).minutes.do(scraper)
+    schedule.every(15).minutes.do(scraper.url2)
+    schedule.every(15).minutes.do(scraper, url1)
     while True:
         schedule.run_pending()
         earnings[:] = []
